120051a2c4cd8ddc40bcb0d3332d91b1551f95aa
Zoltan Burgermeiszter <zoltan.burgermeiszter@gmail.com>
2015-09-22 08:55:04 +0100

    functional-javascript done

--- functional-javascript/17_currying/program.js
+++ functional-javascript/17_currying/program.js
@@ -1,6 +1,22 @@
 function curryN(fn, n) {
-    // SOLUTION GOES HERE
-    return fn(n);
+
+    // n is optional
+    if (typeof n === 'undefined') {
+        n = fn.length
+    }
+
+    // return a function with single argument
+    return function curry(arg) {
+
+        // if the initial curryN callback accepts exactly 1 argument, then return a function call
+        // it is the base case of the recursion
+        if (n === 1) {
+            return fn(arg);
+        }
+
+        // if there are more than 1 arguments then bind the single argument, and run recursion
+        return curryN(fn.bind(this, arg), n-1);
+    }
 }
 
 module.exports = curryN;
